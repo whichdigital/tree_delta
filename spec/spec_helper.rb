d8282c32ef9b52e61b20b29dccaa26c22fa1d705
Chris Patuzzo <chris@patuzzo.co.uk>
2015-01-26 23:29:01 +0000

    Integration test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,6 @@
 require "rspec"
 require "tree_delta"
+
+RSpec.configure do |config|
+  config.color = true
+end
--- spec/tree_delta_spec.rb
+++ spec/tree_delta_spec.rb
@@ -1,7 +1,52 @@
 require "spec_helper"
 
 describe TreeDelta do
-  it "has some tests" do
-    expect(1).to eq(1)
+  class Node
+    attr_reader :id, :children
+
+    def initialize(id, children: [])
+      @id, @children = id, children
+    end
+  end
+
+  it "works for the example in the readme" do
+    alpha = Node.new("alpha", children: [
+      Node.new("a", children: [
+        Node.new("c"),
+        Node.new("d")
+      ]),
+      Node.new("b", children: [
+        Node.new("e")
+      ])
+    ])
+
+    beta = Node.new("beta", children: [
+      Node.new("a", children: [
+        Node.new("d"),
+        Node.new("c")
+      ]),
+      Node.new("e")
+    ])
+
+    delta = TreeDelta.new(from: alpha, to: beta)
+
+    operations = []
+
+    delta.each do |operation|
+      operations << operation
+    end
+
+    expect(operations).to eq [
+      # TODO
+      #
+      # detach 'e'
+      # detach 'd'
+      # detach 'a'
+      # delete 'alpha'
+      # create 'beta' as root
+      # attach 'a' to 'beta' at position 0
+      # attach 'd' to 'a' at position 0
+      # attach 'e' to 'beta' at position 1
+    ]
   end
 end
