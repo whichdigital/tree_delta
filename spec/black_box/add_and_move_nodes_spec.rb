5fa1d93cda28c109c8231018a7a995f7232a03ac
nick-haworth <nick.haworth@which.co.uk>
2015-02-06 14:36:33 +0000

    completed tests for add and move nodes which includes expected failures

--- spec/black_box/add_and_move_nodes_spec.rb
+++ spec/black_box/add_and_move_nodes_spec.rb
@@ -24,7 +24,64 @@ describe TreeDelta do
 
     operations = do_transform(to, from)
 
-    expect(operations.to_a).to eq []
+    expect(operations.to_a).to eq [
+      TreeDelta::Operation.new(
+        type: :detach,
+        id: 'g',
+      ),
+      TreeDelta::Operation.new(
+        type: :detach,
+        id: 'f',
+      ),
+      TreeDelta::Operation.new(
+        type: :detach,
+        id: 'b',
+      ),
+      TreeDelta::Operation.new(
+        type: :detach,
+        id: 'e',
+      ),
+      TreeDelta::Operation.new(
+        type: :detach,
+        id: 'd',
+      ),
+      TreeDelta::Operation.new(
+        type: :attach,
+        id: 'b',
+        parent: 'a',
+        position: 0
+      ),
+      TreeDelta::Operation.new(
+        type: :attach,
+        id: 'd',
+        parent: 'c',
+        position: 0
+      ),
+      TreeDelta::Operation.new(
+        type: :attach,
+        id: 'e',
+        parent: 'c',
+        position: 1
+      ),
+      TreeDelta::Operation.new(
+        type: :attach,
+        id: 'f',
+        parent: 'b',
+        position: 0
+      ),
+      TreeDelta::Operation.new(
+        type: :create,
+        id: 'h',
+        parent: 'f',
+        position: 0
+      ),
+      TreeDelta::Operation.new(
+        type: :attach,
+        id: 'g',
+        parent: 'b',
+        position: 1
+      )
+    ]
   end
 
   it 'can create to a non-leaf node and swap 2 non-leaf nodes' do
@@ -38,7 +95,64 @@ describe TreeDelta do
 
     operations = do_transform(to, from)
 
-    expect(operations.to_a).to eq []
+    expect(operations.to_a).to eq [
+      TreeDelta::Operation.new(
+        type: :detach,
+        id: 'g',
+      ),
+      TreeDelta::Operation.new(
+        type: :detach,
+        id: 'f',
+      ),
+      TreeDelta::Operation.new(
+        type: :detach,
+        id: 'b',
+      ),
+      TreeDelta::Operation.new(
+        type: :detach,
+        id: 'e',
+      ),
+      TreeDelta::Operation.new(
+        type: :detach,
+        id: 'd',
+      ),
+      TreeDelta::Operation.new(
+        type: :attach,
+        id: 'b',
+        parent: 'a',
+        position: 0
+      ),
+      TreeDelta::Operation.new(
+        type: :attach,
+        id: 'd',
+        parent: 'c',
+        position: 0
+      ),
+      TreeDelta::Operation.new(
+        type: :attach,
+        id: 'e',
+        parent: 'c',
+        position: 1
+      ),
+      TreeDelta::Operation.new(
+        type: :attach,
+        id: 'f',
+        parent: 'b',
+        position: 0
+      ),
+      TreeDelta::Operation.new(
+        type: :create,
+        id: 'h',
+        parent: 'b',
+        position: 1
+      ),
+      TreeDelta::Operation.new(
+        type: :attach,
+        id: 'g',
+        parent: 'b',
+        position: 2
+      )
+    ]
   end
 
   it 'can create to a leaf node and move the leaf node to be a child of the root' do
@@ -52,7 +166,24 @@ describe TreeDelta do
 
     operations = do_transform(to, from)
 
-    expect(operations.to_a).to eq []
+    expect(operations.to_a).to eq [
+      TreeDelta::Operation.new(
+        type: :detach,
+        id: 'e'
+      ),
+      TreeDelta::Operation.new(
+        type: :attach,
+        id: 'e',
+        parent: 'a',
+        position: 2
+      ),
+      TreeDelta::Operation.new(
+        type: :create,
+        id: 'h',
+        parent: 'e',
+        position: 0
+      )
+    ]
   end
 
   it 'can create to a non-leaf node and move first sibling be a child of the root' do
@@ -66,7 +197,24 @@ describe TreeDelta do
 
     operations = do_transform(to, from)
 
-    expect(operations.to_a).to eq []
+    expect(operations.to_a).to eq [
+      TreeDelta::Operation.new(
+        type: :detach,
+        id: 'e'
+      ),
+      TreeDelta::Operation.new(
+        type: :create,
+        id: 'h',
+        parent: 'b',
+        position: 1
+      ),
+      TreeDelta::Operation.new(
+        type: :attach,
+        id: 'e',
+        parent: 'a',
+        position: 2
+      )
+    ]
   end
 
   it 'can create to a non-leaf node and move last sibling be a child of the root' do
@@ -80,6 +228,23 @@ describe TreeDelta do
 
     operations = do_transform(to, from)
 
-    expect(operations.to_a).to eq []
+    expect(operations.to_a).to eq [
+      TreeDelta::Operation.new(
+        type: :detach,
+        id: 'd'
+      ),
+      TreeDelta::Operation.new(
+        type: :create,
+        id: 'h',
+        parent: 'b',
+        position: 0
+      ),
+      TreeDelta::Operation.new(
+        type: :attach,
+        id: 'd',
+        parent: 'a',
+        position: 2
+      )
+    ]
   end
 end
